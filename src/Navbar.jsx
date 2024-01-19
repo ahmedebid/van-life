bec2bc9946e8561243b137b5402cfe21c53bd304
ahmedebid <ahmed.ebid.dev@gmail.com>
2024-01-19 14:07:06 +0200

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife
+      </Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
